4e661f4cc653ef4a1b2499bdcfc587eaaa23018b
John <j.plantey@geolid.com>
2016-03-07 16:43:58 +0100

    controller connect

--- app/Controller/Provider.php
+++ app/Controller/Provider.php
@@ -4,25 +4,41 @@ namespace Mooc\Controller;
 
 use Johndodev\Provider\AbstractProvider;
 use Pimple\Container;
+use Silex\Api\BootableProviderInterface;
+use Silex\Api\ControllerProviderInterface;
 use Silex\Application;
+use Silex\ControllerCollection;
 
 /**
  * Les controller en services et le routing
  */
-class Provider extends AbstractProvider
+class Provider extends AbstractProvider implements ControllerProviderInterface, BootableProviderInterface
 {
     public function register(Container $container)
     {
         $container['controller.home'] = function() {
             return new HomeController();
         };
-
-        $this->registerRoutes($container);
     }
 
-    // TODO fix incohérence
-    private function registerRoutes(Application $app)
+    /**
+     * @inheritdoc
+     */
+    public function connect(Application $app)
     {
+        /** @var ControllerCollection $factory */
+        $factory = $app['controllers_factory'];
+
         $app->match('/', 'controller.home:indexAction')->bind('home');
+
+        return $factory;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function boot(Application $app)
+    {
+        $app->mount(null, $this);
     }
 }
