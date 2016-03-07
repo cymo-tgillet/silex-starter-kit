c38c0be87a0c50f082c8181c57b8215157e173c1
John <j.plantey@geolid.com>
2016-03-07 17:10:12 +0100

    user

---
+++ app/Entity/User.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace Mooc\Entity;
+
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * _ORM\EntityListeners({"Mooc\Listener\UserListener"})
+ * @ORM\Table(name="users")
+ */
+class User implements UserInterface
+{
+    /** @ORM\Id @Column(type="integer") @GeneratedValue(strategy="AUTO") **/
+    private $id;
+
+    /** @ORM\Column(type="datetime", name="created_at") **/
+    private $createdAt;
+
+    /** @ORM\Column(type="string", length=64, name="creator_ip") **/
+    private $creatorIp;
+
+    /** @ORM\Column(type="string") **/
+    private $email;
+
+    /** @var  string */
+    private $plainPassword;
+
+    /** @ORM\Column(type="string", name="passwd") **/
+    private $password;
+
+
+    public function __construct()
+    {
+    }
+
+    public static function loadValidatorMetadata(ClassMetadata $metadata)
+    {
+        // Entity
+        $metadata->addConstraints([
+            new UniqueEntity(['fields' => 'email']),
+        ]);
+
+        // Email
+        $metadata->addPropertyConstraints('email', [
+            new Assert\NotBlank(),
+            new Assert\Email(),
+        ]);
+
+        // Password
+        $metadata->addPropertyConstraints('passwd', [
+            new Assert\NotBlank(),
+        ]);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     * @return User
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param mixed $createdAt
+     * @return User
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatorIp()
+    {
+        return $this->creatorIp;
+    }
+
+    /**
+     * @param mixed $creatorIp
+     * @return User
+     */
+    public function setCreatorIp($creatorIp)
+    {
+        $this->creatorIp = $creatorIp;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getRoles()
+    {
+        return array('ROLE_USER');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getSalt()
+    {
+        return null;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getUsername()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function eraseCredentials()
+    {
+        $this->plainPassword = null;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPlainPassword()
+    {
+        return $this->plainPassword;
+    }
+
+    /**
+     * @param string $plainPassword
+     * @return $this
+     */
+    public function setPlainPassword($plainPassword)
+    {
+        $this->plainPassword = $plainPassword;
+        return $this;
+    }
+}
---
+++ app/Listener/UserListener.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Mooc\Listener;
+
+use Mooc\Entity\User;
+use Johndodev\Security\BcryptEncoder;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Pimple\Container;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\Mapping as ORM;
+
+class UserListener
+{
+    private $container;
+
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * on first insert
+     * @ORM\PrePersist
+     * @param User $user
+     * @param LifecycleEventArgs $event
+     */
+    public function PrePersist(User $user, LifecycleEventArgs $event)
+    {
+        /** @var Request $request */
+        $request = $this->container['request_stack']->getCurrentRequest();
+
+        /** @var BcryptEncoder $passwordEncoder */
+        $passwordEncoder = $this->container['security.encoder.digest'];
+
+        // $user->setValidEmailHash(uniqid());
+        $user->setCreatedAt(new \DateTime());
+        $user->setCreatorIp($request->getClientIp());
+        $user->setPassword($passwordEncoder->encodePassword($user->getPlainPassword(), null));
+    }
+}
